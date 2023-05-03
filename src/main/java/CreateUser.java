7694c7366d1376944ac1fbd6dc174e379c4f8d38
Zhanna Nguen <zhanna.nguen@mail.ru>
2023-05-03 16:32:06 +0300

    добавила классы CreateUser,AuthorizationAndRegistration, User & Ingredients

---
+++ src/main/java/CreateUser.java
@@ -0,0 +1,35 @@
+public class CreateUser {
+    private String email;
+    private String password;
+    private String name;
+    public CreateUser(String email, String password, String name){
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+    public CreateUser(){}
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/Ingredients.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Ingredients {
+    private List<String> ingredients;
+
+    public Ingredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
